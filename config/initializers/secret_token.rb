60103b624050504792563b235936c609c3c86ed2
Jake Hartman <jman.hart@live.com>
2014-11-15 18:31:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fotogiraffe::Application.config.secret_key_base = 'b7d2f7a97e582fc83e000419efd48a6465a575e0e1c932e946ea47f8ccb1008dd5ac1fc6f56ccf5ee14c6b423d1dac8bf4b586e2f526b0607ab3228d428b83cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fotogiraffe::Application.config.session_store :cookie_store, key: '_fotogiraffe_session'
