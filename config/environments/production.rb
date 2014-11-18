bd11a9a5a676cad62fc1b65fa5ceb5a5b007abb0
Jake Hartman <jman.hart@live.com>
2014-11-17 23:15:25 -0700

    Added Amazon S3 for Paperclip uploads to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Fotogiraffe::Application.configure do
 
   #Required for Heroku
   config.action_mailer.default_url_options = { :host => 'fotogiraffe.herokuapp.com' }
+
+  # Sets paperclip to upload images to Amazon sc3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
